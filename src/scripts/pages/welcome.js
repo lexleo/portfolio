b6b3819a29914ba2bb6476cf25f017decac0f02e
lexleo <ins227@gmail.com>
2017-09-11 18:14:14 +0300

    radio labels fixed, login-button fadeOut

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -3,27 +3,28 @@ var $ = require('jquery');
 $(document).ready(function(){
 
     $('.login-button').click(function(e){
-        var $this = $(this);
         e.preventDefault();
-        if ($this.text() == 'На главную') {
-            $('#mainBtn').click();
-            $this.text('Авторизоваться');
-            return;
-        }
-        $('#menu').fadeOut(400, function() {
-            $this.text('На главную');
-            $('#auth').fadeIn();
+        console.log('click');
+        $('.login-button, #menu').each(function() {
+            $(this).fadeOut(400, function() {
+                $('#auth').fadeIn(400);
+            });
         });
-        
     });
 
     $('#mainBtn').click(function(e){
         e.preventDefault();
         $('#auth').fadeOut(400, function(){
-            $('#menu').fadeIn();
+            $('.login-button, #menu').each(function() {
+                $(this).fadeIn(400);
+            });
         });
-    
     });
+
+    $(".control-radio > input").on('click', function(){
+        $(this).parent().siblings().addBack().toggleClass("checked");
+    });
+
 
 
 
