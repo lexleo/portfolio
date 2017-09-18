9778d2676ba436e2b62184c46617c2d54135dde6
lexleo <ins227@gmail.com>
2017-09-18 19:57:11 +0300

    map-marker animation

--- docs/assets/scripts/map.js
+++ docs/assets/scripts/map.js
@@ -1,3 +1,4 @@
+// Initialize map
 var map = new mapboxgl.Map({
     container: 'map',
     style: './assets/scripts/style.json',
@@ -5,29 +6,33 @@ var map = new mapboxgl.Map({
     hash: false
 });
 
-// map.addControl(new mapboxgl.NavigationControl());
+// Dizable zoom by Scroll
 map.scrollZoom.disable();
 
-var geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [45.052998577867044, 38.97470441664118]
-    },
-    properties: {
-      title: 'Я здесь',
-      description: 'Шилякин А.М.'
-    }
-  }]
-};
-
+// Define element to draw map-marker in
 var el = document.createElement('div');
 el.className = 'map-marker';
 
+// Add marker
 var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
   .setLngLat([38.97470441664118, 45.052998577867044])
   .addTo(map);
 
+// :hover to animate marker
+document.addEventListener("DOMContentLoaded", function(event) { 
+  
+  var link = document.querySelector('.contacts__link-map');
+  var mapMarker = document.querySelector('.map-marker');
+  
+  
+  link.onmouseover = function(){
+    mapMarker.classList.add("map-marker-up");
+  };
+
+  link.onmouseleave = function(){
+    mapMarker.classList.remove("map-marker-up");
+  };
+
+
+});
 
