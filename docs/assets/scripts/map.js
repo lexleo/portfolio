8ab864f81bd2afb6e67bb4c9b67cee4f342231c1
Alexey Shilyakin <ins227@gmail.com>
2017-09-18 16:56:45 +0300

    marker added on map

--- docs/assets/scripts/map.js
+++ docs/assets/scripts/map.js
@@ -8,8 +8,26 @@ var map = new mapboxgl.Map({
 // map.addControl(new mapboxgl.NavigationControl());
 map.scrollZoom.disable();
 
-var marker = new mapboxgl.Marker()
-  .setLngLat([45.052998577867044,38.97470441664118])
+var geojson = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [45.052998577867044, 38.97470441664118]
+    },
+    properties: {
+      title: 'Я здесь',
+      description: 'Шилякин А.М.'
+    }
+  }]
+};
+
+var el = document.createElement('div');
+el.className = 'map-marker';
+
+var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
+  .setLngLat([38.97470441664118, 45.052998577867044])
   .addTo(map);
 
 
