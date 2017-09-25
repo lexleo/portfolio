bda2a8d7f37733eda61071b99f7012faeddfb96a
Alexey Shilyakin <ins227@gmail.com>
2017-09-25 14:32:10 +0300

    map marker animation come back

--- docs/assets/scripts/map.js
+++ docs/assets/scripts/map.js
@@ -1,56 +1,123 @@
-// Initialize map
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: './assets/scripts/style.json',
-    attributionControl: true,
-    hash: false
-});
-
-// Dizable Zoom by scroll
-map.scrollZoom.disable();
-
-// Define element to draw map-marker in
-var el = document.createElement('div');
-el.className = 'map-marker';
-
-// Add marker
-var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
-  .setLngLat([38.97470441664118, 45.052998577867044])
-  .addTo(map);
-
-// :hover to animate marker
-document.addEventListener("DOMContentLoaded", function(event) { 
+if (window.location.href.endsWith('about.html')) {
   
-  var link = document.querySelector('.contacts__link-map');
-  var mapMarker = document.querySelector('.map-marker');
+  // Initialize map
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: './assets/scripts/style.json',
+      attributionControl: true,
+      hash: false
+  });
   
-  // get transform: translate css parameters
-  function getTranslateXY() {
-    var style = window.getComputedStyle(mapMarker);
-    var matrix = new WebKitCSSMatrix(style.webkitTransform);
-    var translate = {
-      x: +matrix.e,
-      y: +matrix.f
+  // Dizable Zoom by scroll
+  map.scrollZoom.disable();
+  
+  // Define element to draw map-marker in
+  var el = document.createElement('div');
+  el.className = 'map-marker';
+  
+  // Add marker
+  var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
+    .setLngLat([38.97470441664118, 45.052998577867044])
+    .addTo(map);
+  
+  // :hover to animate marker
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    
+    var link = document.querySelector('.contacts__link-map');
+    var mapMarker = document.querySelector('.map-marker');
+    
+    // get transform: translate css parameters
+    function getTranslateXY() {
+      var style = window.getComputedStyle(mapMarker);
+      var matrix = new WebKitCSSMatrix(style.webkitTransform);
+      console.log(matrix);
+      var translate = {
+        x: +matrix.e,
+        y: +matrix.f
+      };
+      return translate;
+    }
+  
+    // Value move on
+    const shift = -15;
+    
+    // move
+    link.onmouseover = function(){
+      var translate = getTranslateXY();
+      var newStyle = "transform: translate(" + translate.x + "px, " + (translate.y + shift) + "px);";
+      mapMarker.style = newStyle;
+      console.log(mapMarker);
     };
-    return translate;
-  }
-
-  // Value move on
-  const shift = -15;
   
-  // move
-  link.onmouseover = function(){
-    var translate = getTranslateXY();
-    var newStyle = "transform: translate(" + translate.x + "px, " + (translate.y + shift) + "px);";                      
-    mapMarker.style = newStyle;
-  };
+    // move back
+    link.onmouseleave = function(){
+      var translate = getTranslateXY();
+      var oldStyle = "transform: translate(" + translate.x + "px, " + (translate.y - shift) + "px);";
+      mapMarker.style = oldStyle;
+    };
+  
+  });
+  
+}
 
-  // move back
-  link.onmouseleave = function(){
-    var translate = getTranslateXY();
-    var oldStyle = "transform: translate(" + translate.x + "px, " + (translate.y - shift) + "px);";                      
-    mapMarker.style = oldStyle;
-  };
 
-});
+
+
+
+
+// // Initialize map
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: './assets/scripts/style.json',
+//     attributionControl: true,
+//     hash: false
+// });
+
+// // Dizable Zoom by scroll
+// map.scrollZoom.disable();
+
+// // Define element to draw map-marker in
+// var el = document.createElement('div');
+// el.className = 'map-marker';
+
+// // Add marker
+// var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
+//   .setLngLat([38.97470441664118, 45.052998577867044])
+//   .addTo(map);
+
+// // :hover to animate marker
+// document.addEventListener("DOMContentLoaded", function(event) { 
+  
+//   var link = document.querySelector('.contacts__link-map');
+//   var mapMarker = document.querySelector('.map-marker');
+  
+//   // get transform: translate css parameters
+//   function getTranslateXY() {
+//     var style = window.getComputedStyle(mapMarker);
+//     var matrix = new WebKitCSSMatrix(style.webkitTransform);
+//     var translate = {
+//       x: +matrix.e,
+//       y: +matrix.f
+//     };
+//     return translate;
+//   }
+
+//   // Value move on
+//   const shift = -15;
+  
+//   // move
+//   link.onmouseover = function(){
+//     var translate = getTranslateXY();
+//     var newStyle = "transform: translate(" + translate.x + "px, " + (translate.y + shift) + "px);";                      
+//     mapMarker.style = newStyle;
+//   };
+
+//   // move back
+//   link.onmouseleave = function(){
+//     var translate = getTranslateXY();
+//     var oldStyle = "transform: translate(" + translate.x + "px, " + (translate.y - shift) + "px);";                      
+//     mapMarker.style = oldStyle;
+//   };
+
+// });
 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
 const welcome = require('./pages/welcome');
 const skills = require('./common/skills');
 const fsMenu = require('./common/fs-menu');
-const map = require('./common/map');
+// const map = require('./common/map');
