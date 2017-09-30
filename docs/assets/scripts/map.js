a0714ee436016546375cbd713275738158995aa7
lexleo <ins227@gmail.com>
2017-09-30 21:57:48 +0300

    map-marker problem

--- docs/assets/scripts/map.js
+++ docs/assets/scripts/map.js
@@ -1,31 +1,34 @@
 if (window.location.href.endsWith('about.html')) {
-  
-  // Initialize map
-  var map = new mapboxgl.Map({
+
+  // :hover to animate marker
+  document.addEventListener("DOMContentLoaded", function (event) {
+
+
+    // Initialize map
+    var map = new mapboxgl.Map({
       container: 'map',
       style: './assets/scripts/style.json',
       attributionControl: true,
       hash: false
-  });
-  
-  // Dizable Zoom by scroll
-  map.scrollZoom.disable();
-  
-  // Define element to draw map-marker in
-  var el = document.createElement('div');
-  el.className = 'map-marker';
-  
-  // Add marker
-  var marker = new mapboxgl.Marker(el, {offset: [-50 / 2, -100 / 2]})
-    .setLngLat([38.97470441664118, 45.052998577867044])
-    .addTo(map);
-  
-  // :hover to animate marker
-  document.addEventListener("DOMContentLoaded", function(event) { 
-    
+    });
+
+    // Dizable Zoom by scroll
+    map.scrollZoom.disable();
+
+    // Define element to draw map-marker in
+    var el = document.createElement('div');
+    el.className = 'map-marker';
+
+    // Add marker
+    var marker = new mapboxgl.Marker(el, {
+        offset: [-50 / 2, -100 / 2]
+      })
+      .setLngLat([38.97470441664118, 45.052998577867044])
+      .addTo(map);
+
     var link = document.querySelector('.contacts__link-map');
     var mapMarker = document.querySelector('.map-marker');
-    
+
     // get transform: translate css parameters
     function getTranslateXY() {
       var style = window.getComputedStyle(mapMarker);
@@ -37,27 +40,27 @@ if (window.location.href.endsWith('about.html')) {
       };
       return translate;
     }
-  
+
     // Value move on
     const shift = -15;
-    
+
     // move
-    link.onmouseover = function(){
+    link.onmouseover = function () {
       var translate = getTranslateXY();
       var newStyle = "transform: translate(" + translate.x + "px, " + (translate.y + shift) + "px);";
       mapMarker.style = newStyle;
       console.log(mapMarker);
     };
-  
+
     // move back
-    link.onmouseleave = function(){
+    link.onmouseleave = function () {
       var translate = getTranslateXY();
       var oldStyle = "transform: translate(" + translate.x + "px, " + (translate.y - shift) + "px);";
       mapMarker.style = oldStyle;
     };
-  
+
   });
-  
+
 }
 
 
@@ -87,10 +90,10 @@ if (window.location.href.endsWith('about.html')) {
 
 // // :hover to animate marker
 // document.addEventListener("DOMContentLoaded", function(event) { 
-  
+
 //   var link = document.querySelector('.contacts__link-map');
 //   var mapMarker = document.querySelector('.map-marker');
-  
+
 //   // get transform: translate css parameters
 //   function getTranslateXY() {
 //     var style = window.getComputedStyle(mapMarker);
@@ -104,7 +107,7 @@ if (window.location.href.endsWith('about.html')) {
 
 //   // Value move on
 //   const shift = -15;
-  
+
 //   // move
 //   link.onmouseover = function(){
 //     var translate = getTranslateXY();
@@ -120,4 +123,3 @@ if (window.location.href.endsWith('about.html')) {
 //   };
 
 // });
-
