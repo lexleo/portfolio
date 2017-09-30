a1329e560e990fb6b0e0ea689d93f7c313d2b71f
lexleo <ins227@gmail.com>
2017-09-30 20:52:13 +0300

    parallax added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,4 @@ const welcome = require('./pages/welcome');
 const skills = require('./common/skills');
 const fsMenu = require('./common/fs-menu');
 const map = require('./common/map');
+const parallax = require('./common/parallax');
