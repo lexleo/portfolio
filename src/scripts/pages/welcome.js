8833b353fd99c284a2c3b31162330067be76eff3
Alexey Shilyakin <ins227@gmail.com>
2017-10-04 16:39:20 +0300

    focus styled

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -1,8 +1,13 @@
+var green = '#00bfa5',
+    darkGreen = '#009688';
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     
     var loginBtn = document.querySelector('.login-button');
     var mainBtn = document.querySelector('#mainBtn');
     var flipContainer = document.querySelector('.flip-container');
+
+    
     
     var flipFlipper = function(e) {
         e.preventDefault();
