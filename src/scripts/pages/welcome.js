a5849bbdb6ef5f3554fb785a590fa5d94275f9d2
Alexey Shilyakin <ins227@gmail.com>
2017-11-08 09:35:19 +0300

    auth from other pages fixed

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -7,14 +7,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var mainBtn = document.querySelector('#mainBtn');
     var flipContainer = document.querySelector('.flip-container');
 
-    
-    
+   
     var flipFlipper = function(e) {
         e.preventDefault();
         flipContainer.classList.toggle("flip");        
     }
     
+    var comeFrom = document.referrer.slice(-10);
+
     if (loginBtn) loginBtn.addEventListener('click', flipFlipper);
     if (mainBtn) mainBtn.addEventListener('click', flipFlipper);
+
+    if (comeFrom !== 'index.html') {
+        setTimeout(function(){
+            flipContainer.classList.toggle("flip");    
+        }, 700);
+    }
 
 });
